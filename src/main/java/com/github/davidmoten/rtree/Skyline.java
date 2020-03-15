b667ef1f46aef0b41e54340c6e5f95780ebbe8d2
Young <erin.chen095@gmail.com>
2020-03-15 00:26:10 -0400

    insert and delete code

--- src/main/java/com/github/davidmoten/rtree/Skyline.java
+++ src/main/java/com/github/davidmoten/rtree/Skyline.java
@@ -1,21 +1,37 @@
 package com.github.davidmoten.rtree;
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Geometry;
+import com.github.davidmoten.rtree.geometry.Point;
 import com.github.davidmoten.rtree.geometry.Rectangle;
 import com.github.davidmoten.rtree.internal.EntryDefault;
 import com.github.davidmoten.rtree.internal.NonLeafDefault;
 import com.github.davidmoten.rtree.internal.LeafDefault;
+import rx.Observable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+import static com.github.davidmoten.rtree.geometry.Geometries.point;
+
 public class Skyline {
     private RTree tree;
     private ArrayList<EntryDefault> SkylineList;
+    private int count;
+    private double x_max;
+    private  double y_max;
     public Skyline(RTree t){
         this.tree = t;
-    }
 
+    }
+    public void setCount(int count){
+        this.count = count;
+    }
+    public void setMaxValue(int x, int y){
+        this.x_max = x;
+        this.y_max = y;
+    }
     public RTree getRtree(){
         return this.tree;
     }
@@ -27,6 +43,17 @@ public class Skyline {
         return Math.sqrt(mbr.x1() * mbr.x1() + mbr.y1() * mbr.y1());
     }
 
+    public int getCount(){
+        return this.count;
+    }
+
+    public double getX_max(){
+        return this.x_max;
+    }
+
+    public double getY_max(){
+        return this.y_max;
+    }
     public void calculateSL(){
         PriorityQueue<ArrayList> pq = new PriorityQueue<>(Comparator.comparingDouble(x -> (double) x.get(0)));
         ArrayList<EntryDefault> S = new ArrayList<>();
@@ -103,11 +130,103 @@ public class Skyline {
         return;
     }
 
-    public void add(){
+    public void add(Point p){
+        ArrayList<EntryDefault> L = new ArrayList<>();
+        ArrayList<EntryDefault> S = getSL();
+        boolean flag = false;
+        for(EntryDefault item: S){
+            if(p.x() > item.geometry().mbr().x1() && p.y() > item.geometry().mbr().y1()){
+                flag = false;
+                break;
+            }
+            else if(p.x() < item.geometry().mbr().x1() && p.y() < item.geometry().mbr().y1()){
+                L.add(item);
+            }
+            flag = true;
+        }
+
+        for(EntryDefault item: L){
+            S.remove(item);
+        }
+        if(flag){
+            this.count += 1;
+            EntryDefault ed = new EntryDefault(getCount(), point(p.x(),p.y()));
+            this.tree.add(getCount(), point(p.x(),p.y()));
+            S.add(ed);
+        }
 
     }
-    public void delete(){
+    public void delete(Point p){
+        Observable leaf = this.tree.search(p);
+        this.tree.delete(leaf, true);
 
+        for(EntryDefault entry: SkylineList){
+            if(entry.geometry().mbr().x1() == p.x() && entry.geometry().mbr().y1() == p.y()){
+                SkylineList.remove(entry);
+                break;
+            }
+        }
+
+        PriorityQueue<Point> pq = ElectionRange(p);
+        ArrayList<Point> L = new ArrayList<>();
+
+        while(!pq.isEmpty()){
+            Point curr_p = pq.poll();
+            boolean flag = true;
+            for(Point item: L){
+                if(curr_p.x() > item.x() && curr_p.y() > item.y()){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag){
+                L.add(curr_p);
+            }
+        }
+
+        for(Point item: L){
+            count += 1;
+            EntryDefault ed = new EntryDefault(getCount(), point(item.x(),item.y()));
+            SkylineList.add(ed);
+        }
+
+
+    }
+
+    public PriorityQueue<Point> ElectionRange(Point p){
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingDouble(x -> (double) Math.sqrt(x.x() + Math.sqrt(x.y()))));
+//        Object obj = tree.entries();
+        Object obj = tree.root().get();
+        for(EntryDefault entry: SkylineList){
+
+            if(entry.geometry().mbr().x1() > p.x() && entry.geometry().mbr().x1() < x_max){
+                x_max = entry.geometry().mbr().x1();
+            }
+            if(entry.geometry().mbr().y1() > p.y() && entry.geometry().mbr().y1() < y_max){
+                y_max = entry.geometry().mbr().y1();
+            }
+
+        }
+
+        FindRangePoint(obj,pq,  p.x(), p.y(), x_max, y_max);
+        return pq;
+    }
+
+    public void FindRangePoint(Object obj, PriorityQueue<Point> pq, double x_0, double y_0, double x_1, double y_1){
+        if(obj.getClass().equals((LeafDefault.class))){
+            double x = ((LeafDefault) obj).geometry().mbr().x1();
+            double y = ((LeafDefault) obj).geometry().mbr().y1();
+            if(x > x_0 && x < x_1 && y > y_0 && y < y_1){
+                Point p = point(x,y);
+                pq.add(p);
+            }
+        }
+        else{
+            for (Object child:((NonLeafDefault) obj).children()){
+                FindRangePoint(child, pq, x_0, y_0, x_1, y_1);
+            }
+        }
     }
 
 }
--- src/test/java/com/github/davidmoten/rtree/chucitest.java
+++ src/test/java/com/github/davidmoten/rtree/chucitest.java
@@ -4,6 +4,7 @@ import com.github.davidmoten.rtree.Skyline;
 import static com.github.davidmoten.rtree.Entries.entry;
 import static com.github.davidmoten.rtree.geometry.Geometries.*;
 
+import com.github.davidmoten.rtree.geometry.Geometries;
 import com.github.davidmoten.rtree.geometry.Geometry;
 import com.github.davidmoten.rtree.geometry.Point;
 import com.github.davidmoten.rtree.geometry.Rectangle;
@@ -25,31 +26,62 @@ public class chucitest {
     }
     @Test
     public void exampletest() throws FileNotFoundException,IOException{
-        File file = new File("/Users/chuci/github/rtree/target/dataset1.txt");
+       // File file = new File("/Users/chuci/github/rtree/target/dataset1.txt");
+        File file = new File("target/dataset1.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
         RTree<Integer, Point> tree = RTree.create();
         int count = 0;
         BufferedReader br2 = new BufferedReader(new FileReader(file));
+        double x_max = Double.MIN_VALUE;
+        double y_max = Double.MIN_VALUE;
         while ((st = br2.readLine()) != null){
             String[] m = st.split(" ");
             double x = Float.parseFloat(m[0]);
             double y = Float.parseFloat(m[1]);
             tree = tree.add(count, point(x, y));
             count++;
+            if(x > x_max) x_max = x;
+            if(y > y_max) y_max = y;
         }
+
         tree.visualize(1000,1000)
                 .save("target/originalTree.png");
         Skyline sl = new Skyline(tree);
+        sl.setCount(count);
         sl.calculateSL();
+
         ArrayList skyline = sl.getSL();
         System.out.println(skyline);
+
+
+
+
         RTree<Integer, Point> new_rtree =  RTree.create();
         for (Object each:skyline){
-            new_rtree = new_rtree.add(1, point(((EntryDefault) each).geometry().mbr().x1(),((EntryDefault) each).geometry().mbr().y1()));
+            new_rtree = new_rtree.add(1, point(((EntryDefault) each).geometry().mbr().x1(),-((EntryDefault) each).geometry().mbr().y1()));
         }
         new_rtree.visualize(1000,1000)
                 .save("target/SkylineTree.png");
+
+        //add point
+        double p_x = 33.9199981689453;
+        double p_y = 20.07999992370605;
+        Point p =  Geometries.point(p_x, p_y);
+        sl.add(p);
+        skyline = sl.getSL();
+        tree = sl.getRtree();
+        System.out.println("========================");
+        System.out.println(skyline);
+
+        //delete
+        System.out.println("========================");
+        int x = tree.size();
+        tree = tree.delete(1,p);
+
+        int s = tree.size();
+
+
 
     }
 }
